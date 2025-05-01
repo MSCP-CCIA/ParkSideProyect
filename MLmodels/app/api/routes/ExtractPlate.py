e7b234a4dffe18baf88313e1a18b44a08220a7f7
Andresmahu <andres.m.hurtado2907@gmail.com>
2025-05-01 13:52:16 -0500

    API extractPalte modified

--- MLmodels/app/api/routes/ExtractPlate.py
+++ MLmodels/app/api/routes/ExtractPlate.py
@@ -1,19 +1,17 @@
 from fastapi import APIRouter, FastAPI, UploadFile, File, HTTPException
 from MLmodels.Inference.ocr_processor import Service
-from app.models.vehicle import SearchVehicleRequest
+import httpx
 import shutil
 import os
-
+from backend.app.models.message import Message
 
 router = APIRouter()
-
-
 
 UPLOAD_DIR = "temp_uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
-@router.post("/extract-plate")
-async def extract_plate(file: UploadFile = File(...)):
+@router.post("/extract-plate",response_model=Message)
+async def extract_plate(file: UploadFile = File(...),)-> Message:
     try:
         temp_path = f"temp_{file.filename}"
         with open(temp_path, "wb") as buffer:
@@ -23,9 +21,18 @@ async def extract_plate(file: UploadFile = File(...)):
         os.remove(temp_path)
 
         if result:
-            return {"plate": result}
+            async with httpx.AsyncClient() as client:
+                response = await client.post(
+                    "http://localhost:8000/flow/entry-register/",
+                    json={"plate": result}
+                )
+            if response.status_code != 200:
+                raise HTTPException(
+                    status_code=response.status_code,
+                    detail=f"Error al registrar la placa: {response.text}"
+                )
+            return Message(message=response.json())
         else:
             raise HTTPException(status_code=422, detail="No se pudo reconocer la placa")
-
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
